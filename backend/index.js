e6b3abf2e6743357ff0fc6a2940e23ee91460c61
Soneesh <gsoneesh725@gmail.com>
2025-03-27 00:14:58 +0530

    added login code for patient and doctor and also added response for homepage

--- backend/index.js
+++ backend/index.js
@@ -25,8 +25,70 @@ pool.connect()
     .then(() => console.log("Connected to PostgreSQL"))
     .catch(err => console.error("Database connection error", err));
 
+//homepage
+app.get('/', (req, res) => {
+    res.render("login.ejs");
+});
 
+// Login Route
+app.post("/login/submit", async (req, res) => {
+    const { username, password,role} = req.body;
+    try {
+        const result = await pool.query(
+            "SELECT * FROM user WHERE username = $1 AND password = $2 AND role=$3",
+            [username, password,role]
+        );
+        if (result.rows.length > 0) {
+            if(req.body["role"]=="patient"){
+                let patientresult;
+                try{
+                     patientresult = await pool.query(
+                        "SELECT * FROM Patient WHERE user_id=$1",
+                        [result.rows[0].user_id]
+                                                            );
+                    }
+                catch(err){
+                    console.log(err);
+                    res.status(500).send("Server error");
+                           }
+                if(patientresult.rows.length>0)
+                    res.render("patient_dashboard.ejs",patientresult.rows);
+                else
+                    console.log("user entry exists but patient table entry missing");
+                } else
 
+                if(req.body["role"]=="doctor"){
+                    let doctorresult;
+                        try{
+                             doctorresult = await pool.query(
+                                "SELECT * FROM Doctor WHERE user_id=$1",
+                                [result.rows[0].user_id]
+                                                                    );
+                            }
+                        catch(err){
+                            console.log(err);
+                            res.status(500).send("Server error");
+                                }
+                    if(doctorresult.rows.length>0)
+                        res.render("doctor.ejs",doctorresult.rows);
+
+                }   else{
+
+                if(req.body["role"]=="admin"){
+                res.render("admin.ejs",{name:username});
+                } else{
+
+                    res.render("reception.ejs");
+                }
+            }
+        } else {
+            res.render("login.ejs",{status:"false"});
+        }
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("Server error");
+    }
+});
     
 
     app.listen(port, () => {
