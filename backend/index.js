a84b7fad9f1a1fb6c69808fca77e53af36633147
Soneesh <gsoneesh725@gmail.com>
2025-03-27 22:35:43 +0530

    added patient dashboard services and reception new patient registration and upto stage 2 appoinment scheduler

--- backend/index.js
+++ backend/index.js
@@ -153,6 +153,93 @@ app.post("/new/patient/register/submit", async (req, res) => {
     }
 });
 
+//appoinment schedule receptionist stage1
+app.post("/new/appoinment/schedule/reception/stage1",async(req,res)=>{
+    const{receptionnistdatarecieved,dob,gender,phone}=req.body;
+    
+    try{
+        let patientquerresult;
+        patientquerresult=await pool.query("SELECT * FROM patient WHERE phone=$1 AND gender=$2 AND dob=$3",
+            [phone,gender,dob]);
+            if(patientquerresult.rows.length>0){
+                res.render("appoinment_scheduler_page_stage2.ejs",{receptiondata:receptionnistdatarecieved,
+                    patientdetails:patientquerresult.rows[0]});
+                    } else{
+                        res.render("reception_dashboard.ejs",{receptionnistdata:receptionnistdatarecieved,patientfound:"false"});
+                    }
+    }
+    catch{
+        console.error("Error in appoinment scheduler patient search");
+        }        
+                                
+});
+
+// appoinment schedule,stage 2 to stage 3 send logic stage 2 we have to confirm the patient details
+app.post("/new/appoinment/schedule/reception/stage2", async (req, res)=>{
+    const {patientdetails,receptionnistdata}=req.body;
+    res.render("appoinment_scheduler_page_stage3.ejs",{patientdata:patientdetails,receptiondata:receptionnistdata});
+});
+
+//appoinment schedule stage 3
+app.post("/new/appoinment/schedule/reception/stage3", async (req, res)=>{
+    const{patiendatarecevied,receptiondata}=req.body;
+    //now here upon choosing a specialist all the doctor names must be given under select option 
+    //ask rithvik how this will be implemtned in frontend
+    //now before deciding anything in the temp database only timing is available maybe under assumtion that
+    //he is available every day also if we want him to be available everyday then maybe keep a limit 
+    //for the number of appoinments he can take in a day and if the count of existing appoinments with that
+    //doctor is already equal to the limit then show to choose another date
+    //now option 2 is no such thing as limit and in terms of availability have days and when the user
+    //gives a date as input we will check what day it is and if the doctor is available or not
+    //if available we will allow for the appoinment to be scheduled otherise we have to give some 
+    //error message in frontend and also we might have to maintain a limit of appoinments for each day
+    //my choice is to go with the first approach with doctor available on all days
+});
+//apoinment token number  page with date and token number and doctor name
+
+//go back to dashboard reception
+app.post("/reception/dashboard", (req, res) => {
+    const{receptionistdata}=req.body;
+    res.render("reception_dashboard.ejs",{receptionnistdata:receptionistdata});
+});
+
+//in patients page  view appoinments
+app.post("/patient/appoinment",async (req,res)=>{
+    const{patientdata}=req.body;
+    try{
+        let patientappoinmentsresult;
+        patientappoinmentsresult=await pool.query("SELECT * FROM appointment WHERE patient_id = $1 ORDER BY date DESC",[patientdata.patient_id]);
+        if(patientappoinmentsresult.rows.length>0)
+        res.render("patient_view_appoinment_page.ejs",{patientdata:patientdata,appoimentsdata:patientappoinmentsresult,noappoinments:"false"});
+    else
+    res.render("patient_view_appoinment_page.ejs",{patientdata:patientdata,noappoinments:"true"});
+    }
+    catch{
+        console.error("Error in patient view appoinment page");
+
+    }
+});
+//in patients page add another option to view all medical records
+app.post("/patients/medical/records",async (req,res)=>{
+    const{patientdata}=req.body;
+    try{
+        let patientmedicalrecordresult;
+        patientmedicalrecordresult=await pool.query("SELECT * FROM medical_record WHERE patient_id = $1 ORDER BY visit_date",patientdata.patient_id);
+        if(patientmedicalrecordresult.rows.length>0)
+        res.render("patient_view_medical_record_page.ejs",{patientdata:patientdata,medicalrecords:patientmedicalrecordresult,norecords:"false"});
+    else
+        res.render("patient_view_medical_record_page.ejs",{patientdata:patientdata,norecords:"true"});
+    }
+    catch{
+        console.error("Error in patient view medical records page");
+    }
+});
+//go back to dashboard patient
+app.post("/patients/dashboard",async (req,res)=>{
+    const{patientdata}=req.body;
+    res.render("patient_dashboard.ejs",{patientdata:patientdata});
+});
+
 //logout
 app.get("/logout", (req, res) => {
     res.redirect("/");
@@ -161,3 +248,9 @@ app.get("/logout", (req, res) => {
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
+//also for doctor he needs to be able to view appoinmets by date change their status and also add medical records
+//also for all pages logout button must be added and go back button reception,doctor,patien,etc
+//also for all pages logout button must be added and go back button reception,doctor,pati
+//alos footer page hospital address,phone number and meet the team link
+//for doctor from view appoinmets we can have appoinments status update and do this by sendin the appoinmets data received here to the next page also that is update appoinment data and then add a medical records also
+//also it might be better to include appoinment id also in medical record table maybe discuss
