91dd8b40e5bd1b6de2f25e663078feaed620dc20
renish_dsouza <renish_b231198cs@nitc.ac.in>
2025-06-18 17:22:37 +0530

    Removed the first attempt

--- search.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE patients (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    age INT NOT NULL,
-    phone VARCHAR(15) NOT NULL,
-    address TEXT NOT NULL,
-    gender VARCHAR(10) NOT NULL,
-    email VARCHAR(100)
-);
